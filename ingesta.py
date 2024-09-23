9863e4405930d869c358d177bbae3f5616748a08
Marcos-1001 <75715909+Marcos-1001@users.noreply.github.com>
2024-09-22 23:03:13 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -1,9 +1,35 @@
 import boto3
+import mysql.connector
+import pandas as pd
 
-ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+
+try: 
+    connection = mysql.connector.connect(host='mysql',
+                                         port='8080',
+                                         database='db_nombres',
+                                         user='mysql',
+                                         password='mysql',
+                                         )
+    sql_select_Query = "select * from datos"
+    cursor = connection.cursor()
+    cursor.execute(sql_select_Query)
+    records = cursor.fetchall()
+    df = pd.DataFrame(records)
+    ficheroUpload = 'datos.csv'
+    df.to_csv(ficheroUpload, index=False)
+    cursor.close()
+    connection.close()
+
+
+except:
+    print("Error de conexión a la base de datos")
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("Conexión cerrada")
 
 s3 = boto3.client('s3')
-response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
+response = s3.upload_file(ficheroUpload, 'data', ficheroUpload)
 
 print("Ingesta completada")
